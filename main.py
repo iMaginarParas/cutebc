ee607e295067d6a92970b0e5f7772ef7453f8470
Paras <parasgupta592003@gmail.com>
2026-07-18 04:20:22 +0530

    cors issue fixed

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ app = FastAPI(title="Prottiva Store API", version="3.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
